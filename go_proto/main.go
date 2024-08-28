374cdbe218f05b6ca4511892d074e124e09f9d9d
FrancescoLuzzi <francescoluzzi99@libero.it>
2024-08-28 02:16:27 +0200

    golang grpc helloworld

---
+++ go_proto/main.go
@@ -0,0 +1,83 @@
+package main
+
+//go:generate go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
+//go:generate go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
+//go:generate protoc --go_out=./ --go-grpc_out=./ --proto_path=../proto ../proto/greeter.proto
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"sync"
+
+	pb "github.com/FrancescoLuzzi/test_proto/go_proto/greeter"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+var (
+	grpc_port = flag.Int("port_grpc", 50051, "The grpc server port")
+	http_port = flag.Int("port_http", 50054, "The http server port")
+	grpc_dst  = flag.String("grpc_dst", "localhost:50053", "Destination grpc server to call")
+)
+
+// server is used to implement helloworld.GreeterServer.
+type server struct {
+	pb.UnimplementedGreeterServer
+}
+
+// SayHello implements helloworld.GreeterServer
+func (s *server) Greet(ctx context.Context, in *pb.GreetRequest) (*pb.GreetReply, error) {
+	log.Printf("Received: %v", in.GetName())
+	return &pb.GreetReply{Answer: "Hello " + in.GetName()}, nil
+}
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	conn, err := grpc.NewClient(*grpc_dst, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Something went wrong while connecting: %v", err), 500)
+		return
+	}
+	client := pb.NewGreeterClient(conn)
+	ctx := context.Background()
+	ret, err := client.Greet(ctx, &pb.GreetRequest{Name: "Hello from Golang"})
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error when requesting: %v", err), 500)
+		return
+	}
+	fmt.Fprintf(w, "%v", ret.Answer)
+}
+
+func main() {
+	flag.Parse()
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *grpc_port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterGreeterServer(s, &server{})
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	go func() {
+		log.Printf("grpc server listening at %v", lis.Addr())
+		defer wg.Done()
+		if err := s.Serve(lis); err != nil {
+			log.Fatalf("failed to serve grpc: %v", err)
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+		http.HandleFunc("/", rootHandler)
+		log.Printf("http server listening at :%d", *http_port)
+		if err := http.ListenAndServe(fmt.Sprintf(":%d", *http_port), nil); err != nil {
+			log.Fatalf("failed to serve http: %v", err)
+		}
+	}()
+	wg.Wait()
+
+}
